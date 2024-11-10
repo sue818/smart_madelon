0a61a229734c1ce1501b43e42f87ae67394be2a2
Mimikyu <mail@mimikyu.dev>
2024-11-10 22:18:53 +0800

    Init ha integration implementation

--- ha_integration/test.py
+++ ha_integration/test.py
@@ -1,5 +1,9 @@
 from pymodbus.client import ModbusTcpClient
-import logging
+from pymodbus import (
+    ExceptionResponse,
+    ModbusException,
+    pymodbus_apply_logging_config,
+)
 
 # Enable logging
 logging.basicConfig()
@@ -7,16 +11,14 @@ log = logging.getLogger()
 log.setLevel(logging.DEBUG)
 
 class ModbusClient:
-    def __init__(self, host='localhost', port=5020):  # Updated port to match the server
+    def __init__(self, host, port=8899):
         self.client = ModbusTcpClient(host=host, port=port)
-    # def __init__(self, host, port=8899):
-        # self.client = ModbusTcpClient(host=host, port=port)
 
     def read_registers(self, start_address, count):
         """Read multiple holding registers using FC03."""
         try:
             self.client.connect()
-            response = self.client.read_holding_registers(start_address, count)
+            response = self.client.read_holding_registers(start_address, count, slave=1)
             if response.isError():
                 print(f"Error reading registers: {response}")
                 return None
@@ -34,20 +36,11 @@ class ModbusClient:
         finally:
             self.client.close()
 
-    def write_multiple_registers(self, start_address, values):
-        """Write multiple registers using FC16."""
-        try:
-            self.client.connect()
-            response = self.client.write_registers(start_address, values)
-            if response.isError():
-                print(f"Error writing registers: {response}")
-        finally:
-            self.client.close()
-
 # Test the functions
 def test_modbus_client():
     host = "localhost"
     # host = "192.168.6.137"
+
     client = ModbusClient(host)
 
     # Test reading registers
@@ -56,15 +49,9 @@ def test_modbus_client():
     print(f"Registers: {registers}")
 
     # Test writing a single register
-    print("Writing a single register:")
-    client.write_single_register(address=0, value=1)
+    # print("Writing a single register:")
+    # client.write_single_register(address=0, value=1)
 
-    # Test writing multiple registers
-    print("Writing multiple registers:")
-    try:
-        client.write_multiple_registers(start_address=0, values=[1, 2, 3, 4, 5])
-    except Exception as e:
-        print(f"Write multiple registers failed: {e}")
 
 if __name__ == "__main__":
     test_modbus_client()
